32b778e7191dfe6f494a246c85dbbedc1349f967
박현아 <167169191+mmmzm@users.noreply.github.com>
2024-09-10 00:51:27 +0900

    Add files via upload

---
+++ sh01_04/src/main/java/com/pcwk/ehr/UserDaoMain.java
@@ -0,0 +1,48 @@
+package com.pcwk.ehr;
+
+import java.sql.SQLException;
+
+public class UserDaoMain {
+
+	UserDao dao;
+	User user;
+	public UserDaoMain() {
+		dao = new UserDao();
+		user= new User("james01","이상무01","4321","2002/12/31");
+	}
+
+	public void doSave() {
+		System.out.println("doSave()");
+		try {
+			int flag=dao.doSave(user);
+			
+			if(1== flag) {
+				System.out.println("doSave 성공");
+			}else {
+				System.out.println("doSave 실패");
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	public void doSelectOne(){
+		System.out.println("doSelectOne()");
+		
+		try {
+			User outVO = dao.doSelectOne(user);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	public static void main(String[] args) {
+		UserDaoMain main=new UserDaoMain();
+		main.doSave();
+		main.doSelectOne();
+	}
+
+}
